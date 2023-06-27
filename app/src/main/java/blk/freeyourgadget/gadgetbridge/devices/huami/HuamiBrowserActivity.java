321869fc07c6a49b4b25053bd9d82c76ab1a0711
bitelaserkhalif <khalif.ops@gmail.com>
2023-06-27 21:44:31 +0700

    add credits

--- app/src/main/java/blk/freeyourgadget/gadgetbridge/devices/huami/HuamiBrowserActivity.java
+++ app/src/main/java/blk/freeyourgadget/gadgetbridge/devices/huami/HuamiBrowserActivity.java
@@ -1,3 +1,22 @@
+/*  Copyright (C) 20223 bitelaserkhalif
+    ------------------------------
+    MODDED Bitelaserkhalif
+    TU9EREVEIEJpdGVsYXNlcmtoYWxpZg
+    ------------------------------
+    This file is part of MODDED Gadgetbridge.
+
+    Gadgetbridge is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gadgetbridge is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 package blk.freeyourgadget.gadgetbridge.devices.huami;
 
 import android.annotation.TargetApi;
